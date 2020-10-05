36208066f569f154a4cd857b39fb3f4f78241933
delfin mongendu <delfdevs@gmail.com>
2020-10-04 21:32:21 -0400

    recursive

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if end is None:
+        end = len(arr) - 1
 
+    if start > end:
+        return -1
+        
+    mid = (start + end) // 2
+
+    if target is arr[mid]:
+        return mid 
+
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid - 1)
+
+    return binary_search(arr, target, mid + 1, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +22,73 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
+def descending_binary_search(arr, target, left, right):
+    # base case 
+    # or we searched the whole array, i.e. when left > right
+    if left > right:
+        return -1
+    # how do we move closer to a base case? 
+    # we'll stop when we either find the target 
+    else:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            # rule out the right side
+            return descending_binary_search(arr, target, left, mid - 1)
+        else:
+            # rule out the left side 
+            return descending_binary_search(arr, target, mid + 1, right)
+
+
 def agnostic_binary_search(arr, target):
     # Your code here
+    # figure out whether the input array is sorted in ascending or descending order 
+    # keep a boolean to indicate the order of the array 
+    # compare arr[0] and arr[1] 
+    if arr[0] > arr[-1]:
+        is_ascending = False
+    else:
+        is_ascending = True
 
+    # if the input array is ascending, call `binary_search` with the array and target 
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    # otherwise, call `descending_binary_search` with the array and target 
+    else:
+        return descending_binary_search(arr, target, 0, len(arr) - 1)
+    
+    
+    
+    
+    
+    # def binary_search(arr, target, start, end):  
+    # # start = 0
+    # # end = n - 1
+    # # Base condition (search space is exhausted)
+    # if start > end:
+    #     return - 1
+    
+    # # we find the mid space in the search space and compares it with key value
+    # mid = (start + end) // 2
+    
+    
+    # # Compare target with the middle element 
+    # # Base condition (key value is found)
+    # if target == arr[mid]:
+    #     # If target matches with the middle element, return the mid index
+    #     return mid
+    
+    #         # if target value is smaller than the middle element in the array,
+    # elif target < arr[mid]:
+    #     # discard all elements in the right search space including the mid element i.e. arr[mid...high]
+    #     return binary_search(arr, start, mid - 1, target)
+    #     # now our new hgh would be mid - 1
+        
+    # else: # if target value is greater than the middle element in the array,
+    #     # discard all elements in the left search space including the mid element i.e. arr[low...mid]
+    #     return binary_search(arr, mid + 1, end, target)
+    #     # now our new low would be mid + 1
+    # # Repeat the process until target is found or the search space is exhausted
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,107 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+# constructs randomized array of length 'size', each element is randomly selected from the range 0 up to 'max'
+def create_array(size=8, max=50):
+    from random import randint
+    return [randint(0, max) for _ in range(size)]
+# print(create_array())
+
+# Since we "divide" and then "conquer", we can think about the total runtime of merge sort as O(Log(N)) * O(N) or 
+# O(N * Log(N))
+
+# The divide and conquer algo splits an array in half, and keeps splitting the list by 2 until it only has singular elements
+# Adjacent elements become sorted pairs, then sorted pairs are merged and sorted with other pairs as well
+# this process continues until we get a sorted list with all the elements of the unsorted input list
+
+# arrA: left array
+# arrB: right array
+# recombination/dividing step: O Log(N) step
+"""Consider an array with 8 elements, when you split the problem, it becomes 2 arrays of 4 elements, then split it again and it becomes 4 arrays of 2 elements. When it finally becomes 8 arrays of 1 element, that's when you join it together but you work your way up combining the 8 arrays of 1 element to 4 arrays of 2 elements, and so forth"""
+# iterative function
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    elements = len(arrA) + len(arrB) # 4 + 4
+    merged_arr = [0] * elements # [0,0,0,0,0,0,0,0] placeholder for the final merged sorted arrays(2)
+    
+    arrA_pointer = arrB_pointer = 0  # initialize both array indeces/pointers into zero because the smallest ends of both arrays will be at the beginning since they are both in sorted order
+    
+    # we use the array lengths often, so it's handy to make variable
+    arrA_length, arrB_length = len(arrA), len(arrB)
 
-    # Your code here
+    # we continue iterating until we've used up all the elements in one of the input arrays
+    for _ in range(elements): # 8 elements of at value of 0 array
+        if arrA_pointer < arrA_length and arrB_pointer < arrB_length: # base case
+            # we check which value from the start of each array is smaller
+            # if the item at the beginning of the left array is smaller or equal,
+            if arrA[arrA_pointer] <= arrB[arrB_pointer]:
+                # then add it to the merged array
+                merged_arr[arrA_pointer + arrB_pointer] = arrA[arrA_pointer]
+                # increment the index for the left array we pulled from to prevent writing the same element onto the merged array again
+                arrA_pointer += 1
 
+            # else if the above is not the case, then there's only another conclusion.
+            else:  # if the item at the beginning of the right array is smaller,
+                # then add it to the merged array
+                merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer]
+                # increment the index for the right array we pulled from to prevent writing the same element onto the merged array again
+                arrB_pointer += 1
 
+        # If the above is not the case, and the for loop does not trigger, we know that there is an element either on the left or right array.
+        # If we've reached the end of the left array, add the elements from the right array to the merged array
+        elif arrA_pointer == arrA_length:
+            merged_arr[arrA_pointer + arrB_pointer] = arrB[arrB_pointer] # right array
+            arrB_pointer += 1
+
+        # If we've reached the end of the right array, add the elements from the left array to the merged array
+        elif arrB_pointer == arrB_length:
+            merged_arr[arrA_pointer + arrB_pointer] = arrA[arrA_pointer] # left array
+            arrA_pointer += 1
+
+    # return result
     return merged_arr
 
+# test case
+a=[1, 3, 5]
+b=[2, 4, 6]
+# print(merge(a, b))
+
 # TO-DO: implement the Merge Sort function below recursively
+# divide and conquer algo == recursion
+# base case is when you have an array with only 1 element
+# to move towards the base case, you must repeatedly divide the original collection in half until we reach the base case
+# arrays with a single element is the base case because they are already sorted
+# then "merge" these sorted pieces back together
+# merging step: O(N) step
 def merge_sort(arr):
-    # Your code here
+    # if the length of the array is a single element, return it
+    # because an array of zero or one element is already sorted, by definition
+    if len(arr) <= 1: return arr
+    
+    # split the array in half and call merge sort recursively on each half
+    # use floor division to get the midpoint, indices must be integers
+    midpoint = int(len(arr) // 2)
+    
+    # sort and merge each half
+    # recursive calls 
+    arrA = merge_sort(arr[:midpoint]) # left half of the array will be up to the midpoint
+    arrB = merge_sort(arr[midpoint:]) # right half of the array will be from midpoint to the end
+    
+    # merge the sorted arrays (left & right) into a new one
+    return merge(arrA, arrB)
+    
+
+# verify it works
+a = create_array()
+b = create_array()
+print(a)
+print(b)
+
+s = merge_sort(a + b) 
+print(s)
 
 
-    return arr
+
+
+
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +109,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
